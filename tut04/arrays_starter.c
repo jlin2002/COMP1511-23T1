8e444a2864c68f63c19fb4fe7cb33244d746adcf
Joanna Lin <linjoanna31@gmail.com>
2023-03-09 15:05:59 +1100

    Added arrays starter code

---
+++ tut04/arrays_starter.c
@@ -0,0 +1,17 @@
+// A demonstration of arrays
+
+// <CLASS>, <MONTH YEAR>
+
+#include <stdio.h>
+#define SIZE 10
+
+int main(void) {
+    // Task 1: Create an integer array called 'numbers' of size SIZE.
+    // Initialise all elements to 0.
+    // Task 2: Print the elements of the array.
+    // Task 3: Set the elements of the array to 1, 2, 3...
+    // Task 4: Print the elements of the array.
+    // Task 5: Create a function that prints elements of an array and use it.
+
+    return 0;
+}
